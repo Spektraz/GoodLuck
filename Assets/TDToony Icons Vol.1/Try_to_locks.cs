e69a2bf4c36fe56eafb86c80040971a800f5058d
Vlad Fotesko <foteskin18@gmail.com>
2019-12-11 00:07:06 +0200

    first commit

---
+++ Assets/TDToony Icons Vol.1/Try_to_locks.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Try_to_locks : MonoBehaviour
+{
+    public GameObject gameObject;
+	void Start () {
+		
+	}
+	void Update () {
+	    if (Input.GetMouseButton(0))
+	    {
+	        Debug.Log("Work");
+	        transform.Rotate(Vector3.up, Time.deltaTime * 13);
+            
+	    }
+	}
+}
